22ff89aac4b7d2708d34c621c5f3fa8f59ac98fd
imchipwood <imchipwood@gmail.com>
2015-05-22 21:03:12 -0700

    using a class for test cases now, reconverted all existing tests to this format

--- sdr_ctrl/verif/tb/top_hvl.sv
+++ sdr_ctrl/verif/tb/top_hvl.sv
@@ -48,100 +48,207 @@
 
 `timescale 1ns/1ps
 
-// This testbench verify with SDRAM TOP
+// class for storing and manipulating test cases
+class TestCase;
+    logic [31:0] address;
+    logic  [7:0] [31:0] data;
+    logic  [7:0] bl; // burst length
+    logic [19:0] row;
+    logic  [1:0] bank;
+    logic  [7:0] column;
+    
+    function new(logic [19:0] row, logic [1:0] bank, logic [7:0] column, logic [7:0] bl);
+        int i = 0;
+        this.row        = row;
+        this.bank       = bank;
+        this.column     = column;
+        this.bl         = bl;
+        for (i = 0; i < 8; i++) begin
+            if (i < this.bl)
+                this.data[i]       = $random & 32'hFFFFFFFF;
+        end
+        this.address    = {row,bank,column,2'b0};
+    endfunction
+    
+    function void setAddress(logic [31:0] address);
+        this.address = address & 32'h00FF_FFFC;
+        this.row = address[31:12];
+        this.bank = address[11:10];
+        this.column = address[9:2];
+    endfunction
+    
+    function logic [31:0] getAddress();
+        return this.address;
+    endfunction
+    
+    function logic [7:0] [31:0] getData();
+        return this.data;
+    endfunction
+    
+    function void setBL(logic [7:0] bl);
+        this.bl = bl;
+    endfunction
+    
+    function logic [7:0] getBL();
+        return this.bl;
+    endfunction
+    
+    function void print();
+        string data, d;
+        int i;
+        for (i = 7; i >= 0; i--) begin          // pretty print prints as integers... this formats to hex for smaller prints
+            $sformat(d, "%8h", this.data[i]);
+            data = {data, ", ", d};
+        end
+        $display("Transaction - address=%h, row=%5h, bank=%1d, column=%2h, bl=%3h", this.address, this.row, this.bank, this.column, this.bl);
+        $display("Transaction - data = %s", data);
+    endfunction
+
+endclass
+
+
+
+
+
+
+
 
 module top_hvl #(
-    parameter P_SYS  = 10,     //    200MHz
-    parameter P_SDR  = 20,     //    100MHz
-    parameter CFG_SDR_WIDTH = 2'b10,
-    parameter CFG_COLBITS   = 2'b00,
-    parameter TWR       = 1, // Write Recovery
-    parameter TRAS_D    = 4, // Active to Precharge Delay
-    parameter TCAS      = 3, // CAS Latency
-    parameter TRCD_D    = 2, // Active to Read or Write Delay
-    parameter TRP_D     = 2, // Precharge Command Period
-    parameter TRCAR_D   = 7, // Active-Active/Auto-Refresh Command Period
-    parameter BURST_LEN = 3  // READ/WRITE Burst Length
 )
 ();
 
-//--------------------
-// data/address/burst length FIFO
-//--------------------
-int dfifo[$]; // data fifo
-int afifo[$]; // address  fifo
-int bfifo[$]; // Burst Length fifo
+TestCase tcfifo[$]; // queue to hold test cases currently executing
+
+// variables to use throughout testbench 
+TestCase t;
+logic [19:0] row;
+logic  [1:0] bank;
+logic  [7:0] column;
+logic [31:0] address;
+logic  [7:0] bl;
+longint ErrCnt;
+int i, k;
 
 // Initialize Configuration Parameters
 initial begin
     top_hdl.cfg.setup();
 end
 
-reg [31:0] read_data;
-reg [31:0] ErrCnt;
-int k;
-reg [31:0] StartAddr;
 /////////////////////////////////////////////////////////////////////////
 // Test Case
 /////////////////////////////////////////////////////////////////////////
-
 initial begin
     
-
     $display("Waiting for reset");
     top_hdl.wbi.waitForReset();
     $display("Reset finished");
     
 
-   ErrCnt          = 0;
+   ErrCnt = 0;
     
-  //wait(top_hdl.cfg.sdr_init_done === 1);
   @(posedge top_hdl.cfg.sdr_init_done) $display("SDR Init done");
 
   $display("-------------------------------------- ");
   $display(" Case-1: Single Write/Read Case        ");
   $display("-------------------------------------- ");
 
-  burst_write(32'h4_0000,8'h4);
-
+  t = new(.row(12'h100), .bank(0), .column(0), .bl(8'h4));
+  burst_write(t);
   burst_read();
+  
 
   // Repeat one more time to analysis the
   // SDRAM state change for same col/row address
   $display("-------------------------------------- ");
   $display(" Case-2: Repeat same transfer once again ");
   $display("----------------------------------------");
-  burst_write(32'h4_0000,8'h4);
+  t = new(.row(20'h100), .bank(0), .column(0), .bl(8'h4));
+  burst_write(t);
   burst_read();
-  burst_write(32'h0040_0000,8'h5);
+  t = new(.row(20'h100), .bank(0), .column(0), .bl(8'h4));
+  burst_write(t);
   burst_read();
+  
+  
   $display("----------------------------------------");
-  $display(" Case-3 Create a Page Cross Over        ");
+  $display(" Case-3: Create a Page Cross Over        ");
   $display("----------------------------------------");
-  burst_write(32'h0000_0FF0,8'h8);
-  burst_write(32'h0001_0FF4,8'hF);
-  burst_write(32'h0002_0FF8,8'hF);
-  burst_write(32'h0003_0FFC,8'hF);
-  burst_write(32'h0004_0FE0,8'hF);
-  burst_write(32'h0005_0FE4,8'hF);
-  burst_write(32'h0006_0FE8,8'hF);
-  burst_write(32'h0007_0FEC,8'hF);
-  burst_write(32'h0008_0FD0,8'hF);
-  burst_write(32'h0009_0FD4,8'hF);
-  burst_write(32'h000A_0FD8,8'hF);
-  burst_write(32'h000B_0FDC,8'hF);
-  burst_write(32'h000C_0FC0,8'hF);
-  burst_write(32'h000D_0FC4,8'hF);
-  burst_write(32'h000E_0FC8,8'hF);
-  burst_write(32'h000F_0FCC,8'hF);
-  burst_write(32'h0010_0FB0,8'hF);
-  burst_write(32'h0011_0FB4,8'hF);
-  burst_write(32'h0012_0FB8,8'hF);
-  burst_write(32'h0013_0FBC,8'hF);
-  burst_write(32'h0014_0FA0,8'hF);
-  burst_write(32'h0015_0FA4,8'hF);
-  burst_write(32'h0016_0FA8,8'hF);
-  burst_write(32'h0017_0FAC,8'hF);
+  bl = 8'h8;
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0000_0FF0);
+  burst_write(t);
+  // call new in between each write in order to regenerate data
+  bl = 8'hF;
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0001_0FF4);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0002_0FF8);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0003_0FFC);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0004_0FE0);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0005_0FE4);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0006_0FE8);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0007_0FEC);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0008_0FD0);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0009_0FD4);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h000A_0FD8);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h000B_0FDC);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h000C_0FC0);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h000D_0FC4);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h000E_0FC8);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h000F_0FCC);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0010_0FB0);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0011_0FB4);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0012_0FB8);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0013_0FBC);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0014_0FA0);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0015_0FA4);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0016_0FA8);
+  burst_write(t);
+  t = new(0,0,0,bl);
+  t.setAddress(32'h0017_0FAC);
+  burst_write(t);
+  
   burst_read();
   burst_read();
   burst_read();
@@ -166,93 +273,89 @@ initial begin
   burst_read();
   burst_read();
   burst_read();
+  
 
   $display("----------------------------------------");
-  $display(" Case:4 4 Write & 4 Read                ");
+  $display(" Case-4: 4 Write & 4 Read                ");
   $display("----------------------------------------");
-  burst_write(32'h4_0000,8'h4);
-  burst_write(32'h5_0000,8'h5);
-  burst_write(32'h6_0000,8'h6);
-  burst_write(32'h7_0000,8'h7);
+  row = 20'h00010;
+  bank = 0;
+  column = 0;
+  bl = 8'h4;
+  
+  for (i = 0; i < 4; i++) begin
+    if (i > 0)
+        row = row + 16;
+    t = new(row, bank, column, bl++);
+    burst_write(t);
+  end
+  
   burst_read();
   burst_read();
   burst_read();
   burst_read();
+  
 
   $display("---------------------------------------");
-  $display(" Case:5 24 Write & 24 Read With Different Bank and Row ");
+  $display(" Case-5: 24 Write & 24 Read With Different Bank and Row ");
   $display("---------------------------------------");
-  //----------------------------------------
-  // Address Decodeing:
-  //  with cfg_col bit configured as: 00
-  //    <12 Bit Row> <2 Bit Bank> <8 Bit Column> <2'b00>
-  //
-  burst_write({12'h000,2'b00,8'h00,2'b00},8'h4);   // Row: 0 Bank : 0
-  burst_write({12'h000,2'b01,8'h00,2'b00},8'h5);   // Row: 0 Bank : 1
-  burst_write({12'h000,2'b10,8'h00,2'b00},8'h6);   // Row: 0 Bank : 2
-  burst_write({12'h000,2'b11,8'h00,2'b00},8'h7);   // Row: 0 Bank : 3
-  burst_write({12'h001,2'b00,8'h00,2'b00},8'h4);   // Row: 1 Bank : 0
-  burst_write({12'h001,2'b01,8'h00,2'b00},8'h5);   // Row: 1 Bank : 1
-  burst_write({12'h001,2'b10,8'h00,2'b00},8'h6);   // Row: 1 Bank : 2
-  burst_write({12'h001,2'b11,8'h00,2'b00},8'h7);   // Row: 1 Bank : 3
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-
-  burst_write({12'h002,2'b00,8'h00,2'b00},8'h4);   // Row: 2 Bank : 0
-  burst_write({12'h002,2'b01,8'h00,2'b00},8'h5);   // Row: 2 Bank : 1
-  burst_write({12'h002,2'b10,8'h00,2'b00},8'h6);   // Row: 2 Bank : 2
-  burst_write({12'h002,2'b11,8'h00,2'b00},8'h7);   // Row: 2 Bank : 3
-  burst_write({12'h003,2'b00,8'h00,2'b00},8'h4);   // Row: 3 Bank : 0
-  burst_write({12'h003,2'b01,8'h00,2'b00},8'h5);   // Row: 3 Bank : 1
-  burst_write({12'h003,2'b10,8'h00,2'b00},8'h6);   // Row: 3 Bank : 2
-  burst_write({12'h003,2'b11,8'h00,2'b00},8'h7);   // Row: 3 Bank : 3
-
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-
-  burst_write({12'h002,2'b00,8'h00,2'b00},8'h4);   // Row: 2 Bank : 0
-  burst_write({12'h002,2'b01,8'h01,2'b00},8'h5);   // Row: 2 Bank : 1
-  burst_write({12'h002,2'b10,8'h02,2'b00},8'h6);   // Row: 2 Bank : 2
-  burst_write({12'h002,2'b11,8'h03,2'b00},8'h7);   // Row: 2 Bank : 3
-  burst_write({12'h003,2'b00,8'h04,2'b00},8'h4);   // Row: 3 Bank : 0
-  burst_write({12'h003,2'b01,8'h05,2'b00},8'h5);   // Row: 3 Bank : 1
-  burst_write({12'h003,2'b10,8'h06,2'b00},8'h6);   // Row: 3 Bank : 2
-  burst_write({12'h003,2'b11,8'h07,2'b00},8'h7);   // Row: 3 Bank : 3
-
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
-  burst_read();
+  // loop through each row, within each row loop through each bank
+  // start with burst length of 4, increasing by 1 each time, resetting to 4 every 4th write
+  row = 0;
+  bank = 0;
+  column = 0;
+  bl = 4;
+  for (i = 0; i < 16; i++) begin
+    if (i % 4 == 0 && i > 0) begin
+        row++;
+        bl = 0;
+    end
+    t = new(row, bank, column, bl);
+    burst_write(t);
+    bl++;
+    bank++;
+  end
+  for (i = 0; i < 16; i++) begin
+    burst_read();
+  end
+  
+  // same thing but increment column each time
+  row = 2;
+  bank = 0;
+  column = 0;
+  bl = 4;
+  for (i = 0; i < 8; i++) begin
+    if (i % 4 == 0 && i > 0) begin
+        row++;
+        bl = 0;
+    end
+    t = new(row, bank, column, bl);
+    burst_write(t);
+    bl++;
+    bank++;
+    column++;
+  end
+  for (i = 0; i < 8; i++) begin
+    burst_read();
+  end
+  
   $display("---------------------------------------------------");
-  $display(" Case: 6 Random 2 write and 2 read random");
+  $display(" Case-6: Random 2 write and 2 read random");
   $display("---------------------------------------------------");
   for(k=0; k < 20; k++) begin
-     StartAddr = $random & 32'h003FFFFF;
-     burst_write(StartAddr,($random & 8'h0f)+1);
-     StartAddr = $random & 32'h003FFFFF;
-     burst_write(StartAddr,($random & 8'h0f)+1);
+     t = new(0,0,0,($random & 8'h0f)+1);
+     t.setAddress($random & 32'h003FFFFF);
+     burst_write(t);
+     
+     t = new(0,0,0,($random & 8'h0f)+1);
+     t.setAddress($random & 32'h003FFFFF);
+     burst_write(t);
+     
      burst_read();
      burst_read();
   end
   
   
-
 
         $display("###############################");
     if(ErrCnt == 0)
@@ -265,39 +368,35 @@ end
 
 
 task burst_write;
-   input [31:0] Address;
-   input [7:0]  bl;
+   input TestCase tc;
    int i;
+   logic [31:0] d;
+   
+   tcfifo.push_back(tc);
+   tc.print();
 
-   automatic logic [31:0] data = $random & 32'hFFFFFFFF;
-
-   afifo.push_back(Address);
-   bfifo.push_back(bl);
-
-   for(i=0; i < bl; i++) begin
-
-      dfifo.push_back(data);
-      $display("tb_top:  Status: Burst-No: %d  Write Address: %x  WriteData: %x ",i,Address,data);
-      top_hdl.wbi.write(Address[31:2]+i, bl, data);
+   for(i = 0; i < tc.getBL(); i++) begin
+      d = tc.getData()[i];
+      $display("tb_top:  Status: Burst-No: %d  Write Address: %x  WriteData: %x ", i, tc.getAddress(), d);
+      top_hdl.wbi.write(tc.getAddress()[31:2]+i, tc.getBL(), d);
    end
 
 endtask
 
 task burst_read();
-   automatic logic [31:0] address = afifo.pop_front();
-   automatic logic  [7:0] bl      = bfifo.pop_front();
-   logic [31:0] exp_data, data;
-   int j;
-
-   for(j=0; j < bl; j++) begin
-      $display("tb_top:  Read Address: %x, Burst Size: %d",address,bl);
-      top_hdl.wbi.read(address[31:2]+j, bl, data);
-      exp_data = dfifo.pop_front();
-      if (data !== exp_data) begin
-          $display("tb_top:  READ ERROR: Burst-No: %d Addr: %x Rxp: %x Exd: %x",j,address,data,exp_data);
+   automatic TestCase tc = tcfifo.pop_front();
+   logic [31:0] data, d;
+   int i;
+   
+   for(i = 0; i < tc.getBL(); i++) begin
+      $display("tb_top:  Read Address: %x, Burst Size: %d", tc.getAddress(), tc.getBL());
+      top_hdl.wbi.read(tc.getAddress()[31:2]+i, tc.getBL(), data);
+      d = tc.getData()[i];
+      if (data !== d) begin
+          $display("tb_top:  READ ERROR: Burst-No: %d Addr: %x Rxp: %x Exd: %x", i, tc.getAddress(), data, d);
           ErrCnt = ErrCnt+1;
       end else begin
-          $display("tb_top:  READ STATUS: Burst-No: %d Addr: %x Rxd: %x",j,address,data);
+          $display("tb_top:  READ STATUS: Burst-No: %d Addr: %x Rxd: %x", i, tc.getAddress(), data);
       end
    end
 
